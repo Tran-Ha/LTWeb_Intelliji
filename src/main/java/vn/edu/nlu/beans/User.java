0c566e6dec73f0d996fbedab168588adbec6f324
VDTan <18130209@st.hcmuaf.edu.vn>
2020-12-24 19:43:11 +0700

    Tan commit

---
+++ src/main/java/vn/edu/nlu/beans/User.java
@@ -0,0 +1,138 @@
+package vn.edu.nlu.beans;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String address;
+    private String city;
+    private String img;
+    private Date birthday;
+    private boolean active;
+    private boolean isAdmin;
+
+    public User(){}
+
+    public User(int id, String name, String email, String phone, String password, String address, String city, String img, Date birthday, boolean active, boolean isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.address = address;
+        this.city = city;
+        this.img = img;
+        this.birthday = birthday;
+        this.active = active;
+        this.isAdmin = isAdmin;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", img='" + img + '\'' +
+                ", birthday=" + birthday +
+                ", active=" + active +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+}
--- src/main/java/vn/edu/nlu/database/ConnectionDB.java
+++ src/main/java/vn/edu/nlu/database/ConnectionDB.java
@@ -1,19 +1,16 @@
 package vn.edu.nlu.database;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class ConnectionDB {
     static Connection connection;
 
     public static Statement connect() throws ClassNotFoundException, SQLException {
-        if (connection == null || connection.isClosed()){
-            Class.forName("com.mysql.jdbc.Driver");
+        if(connection == null || connection.isClosed()){
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + "ZoeDatabase?useUnicode=true&characterEncoding=utf-8", "root", "");
-            return connection.createStatement();
-        } else {
-            return connection.createStatement();
         }
+        return connection.createStatement();
     }
+
 }
